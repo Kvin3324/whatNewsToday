52a8d54cb3095ecbfe09102966587a5ed0f59518
Kévin Joya <kevin.joya@outlook.com>
2019-10-02 19:16:23 +0200

    Added container and changed col art everything

--- src/App.js
+++ src/App.js
@@ -9,14 +9,16 @@ import './App.css';
 function App() {
   return (
     <div className="App col-12">
+      <div className="container--app">
       <Header />
       <div className="row">
-        <div className="col-9 articles--everything">
+        <div className="col-8 articles--everything">
           <MainArticles />
         </div>
         <div className="col-3 articles--headlines">
           <HeadlineArticles />
         </div>
+      </div>
       </div>
     </div>
   );
