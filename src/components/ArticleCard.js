3df65467d8fbe5a3bcda4725f8b984dd390fde85
Kévin Joya <kevin.joya@outlook.com>
2019-10-02 19:16:59 +0200

    changed front and added some logic

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -8,26 +8,42 @@ function ArticleCard(props) {
           <div className="card">
             <div className="card-body">
               <div className="card--img">
-                <img src={`${props.urlToImage}`} className="card-img-list" alt="..."/>
+                {
+                  props.urlToImage === null ?
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/No_image_available_600_x_450.svg/600px-No_image_available_600_x_450.svg.png" className="card-img-list" alt="..."/>
+                    : <img src={`${props.urlToImage}`} className="card-img-list" alt="..."/>
+                }
               </div>
-              <div className="row mb-4">
+              <div className="row mb-2">
                 <div className="card--date col-6">
                   <Moment format="DD/MM/YYYY">
                     {`${props.publishedAt}`}
                   </Moment>
                 </div>
                 <div className="card--author col-6">
-                  <p>{props.author}</p>
+                  <p className="card--author--name">{props.author}</p>
                 </div>
               </div>
                 <h5 className="card-title">{props.title}</h5>
-                <p>{props.description}</p>
+                <div className="card-description">
+                  {
+                    props.description && props.description.length > 40 ?
+                      <p>{props.description.substr(0, 100)}... <a href={`${props.url}`} target="blank"> Read more</a></p> :
+                      <p>{props.description}</p>
+                  }
+                </div>
               <div className="card--source">
-                <p>{props.source}</p>
+                <strong>
+                  <p>{props.source}</p>
+                </strong>
               </div>
-              <div className="card--url">
-                <a href={`${props.url}`}>{props.url}</a>
-              </div>
+              {/* <div className="card--url">
+                {
+                  props.url.length > 40 ?
+                  <a href={`${props.url}`} target="blank">{`${props.url.substr(0, 40)}(...)`}</a> :
+                  <a href={`${props.url}`} target="blank">{props.url}</a>
+                }
+              </div> */}
             </div>
           </div>
         // </div>
