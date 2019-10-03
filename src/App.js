7f6b25ad8426c28aa978b438cf51792de0d31c55
Kévin Joya <kevin.joya@outlook.com>
2019-10-03 15:41:45 +0200

    Cleaned code

--- src/App.js
+++ src/App.js
@@ -2,23 +2,21 @@ import React from 'react';
 import Header from "./components/Header";
 import MainArticles from "./components/MainArticles";
 import HeadlineArticles from "./components/HeadlineArticles";
-
-// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App col-12">
       <div className="container--app">
-      <Header />
-      <div className="row">
-        <div className="col-8 articles--everything">
-          <MainArticles />
+        <Header />
+        <div className="row">
+          <div className="col-8 articles--everything">
+            <MainArticles />
+          </div>
+          <div className="col-3 articles--headlines">
+            <HeadlineArticles />
+          </div>
         </div>
-        <div className="col-3 articles--headlines">
-          <HeadlineArticles />
-        </div>
-      </div>
       </div>
     </div>
   );
