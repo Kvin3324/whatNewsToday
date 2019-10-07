d442f89c083e21055f0ec0d8a7d6b9082b6d834d
Kévin Joya <kevin.joya@outlook.com>
2019-10-07 11:56:26 +0200

    Added folder to deploy

---
+++ docs/precache-manifest.27e8636b37848cbb0fdff001e6e80816.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26232bc8e85ff3611ed31af5aa7ce7e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fee69316ad5bcef3aaf",
+    "url": "/static/css/main.2929ffeb.chunk.css"
+  },
+  {
+    "revision": "b02f71816137a944a1c7",
+    "url": "/static/js/2.42df74c9.chunk.js"
+  },
+  {
+    "revision": "9fee69316ad5bcef3aaf",
+    "url": "/static/js/main.4f978fb1.chunk.js"
+  },
+  {
+    "revision": "d5649cb5ba95757d7202",
+    "url": "/static/js/runtime-main.f8c1043c.js"
+  },
+  {
+    "revision": "759573e11a5e10f3e75b30c7a37d02a3",
+    "url": "/static/media/Olondon_.759573e1.otf"
+  }
+]);
